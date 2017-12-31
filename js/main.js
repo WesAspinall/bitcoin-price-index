09483ad6874cf7664c289113c6816f9d0641e2e4
wesaspinall <wes@wesaspinall.com>
2017-12-31 04:26:44 -0500

    fetched index data

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 (function(){
-  console.log('hello');
+
+  const URL = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+
+  const root = document.querySelector('#root');
+
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => {
+        bpi = data.bpi;
+
+        bpiInfo = Object.values(bpi);
+
+        html = bpiInfo.map((item) => {
+          return `
+           <div class='currency'>
+           <ul>
+             <li>${item.code} (${item.symbol})</li>
+             <li>Rate: ${item.rate}</li>
+             <li>Rate Float: ${item.rate_float}</li>
+           </ul>
+           </div>
+          `;
+
+        }).join('');
+        
+        root.innerHTML = html;
+    });
+
 })();
